8367cb7d8fe8e2ffdc88d8d23feb22dc759b9d45
phil-henning <phenning@apple.com>
2024-06-02 12:41:20 -0600

    Adding data loader file

---
+++ scripts/data_loaders.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import JSONLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from scripts.utilities import merge_json_files
+
+
+text_splitter = RecursiveCharacterTextSplitter()
+
+
+def load_multifile_jsondata(folder_path, jq_schema):
+    docs = merge_json_files(folder_path)    
+    loader = JSONLoader(
+        file_path=f"{folder_path}/merged_data.json",
+        jq_schema=jq_schema,
+        text_content=False
+    )
+    doc = loader.load()
+    return text_splitter.split_documents(doc)
+
+
+def load_data_jsonfile(file_path, jq_schema):
+    loader = JSONLoader(
+        file_path=file_path,
+        jq_schema=jq_schema,
+        text_content=False
+    )
+    doc = loader.load()
+    return text_splitter.split_documents(doc)
+
+def load_multifile_unstruct_markdown(folder_path):
+    pass
